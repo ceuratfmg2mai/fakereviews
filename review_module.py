848fda1b8b70785f7b8a262973cd6feaa75d6426
aagudelom <alejandro.agudelo@gmail.com>
2025-06-01 15:17:11 +0200

    Update review_module.py

--- review_module.py
+++ review_module.py
@@ -1,5 +1,5 @@
 from typing import Dict, Any
-
+from qdrant_connection import QdrantDataIngestor
 class ReviewFinder:
     """
     Clase para crear el prompt de las reseñas test.
@@ -17,7 +17,7 @@ class ReviewFinder:
             str: Prompt cumplimentado con los datos de la fila.
         """
         prompt = prompt_template.format(
-            text=row.get("text", ""),
+            text=row.get("text", "").replace('\n', ' '),
             review_count_user=row.get("review_count_user", ""),
             average_stars_user=row.get("average_stars_user", ""),
             yelping_days=row.get("yelping_days", ""),
@@ -27,3 +27,40 @@ class ReviewFinder:
             fans=row.get("fans", "")
         )
         return prompt
+    
+    def format_prompt_for_row(row_data: dict, ingestor:QdrantDataIngestor, prompt_template_example: str, base_prompt_template: str) -> str:
+        """
+        Toma un diccionario (row_data) con los datos de la fila y rellena la plantilla.
+        Maneja valores nulos convirtiéndolos a string (p.ej., 'None').
+        """
+        # Crea una copia o un nuevo diccionario para asegurar que todos los valores sean strings
+        # y manejar los nulos de forma segura para .format()
+        data_for_format = {}
+        final_prompt = None
+        separator = ", "
+        for key, value in row_data.items():
+            if key == 'text':
+                if not value:
+                    return f"ERROR: El texto de la reseña no puede ser vacio"
+                else:
+                    # Busqueda por similitud en el rag
+                    reviews = ingestor.get_review_by_text(str(value).replace('\n', ' '))
+                    if reviews:
+                        # creación del prompt example
+                        prompts = ingestor.build_prompts_from_reviews(reviews, prompt_template_example)
+                        data_for_format['reviews_examples_with_metadata'] = separator.join(prompts)                        
+                    else:
+                        return f"ERROR: No se encontraron reseñas de ejemplo en el RAG para la reseña: {value[:50]}..."        
+            else:
+                data_for_format['review_test'] = value     
+
+        try:
+            print(data_for_format)
+            return base_prompt_template.format(**data_for_format)
+        except KeyError as e:
+            # Error útil si falta un placeholder en los datos de la fila
+            print(f"Error: Falta la clave {e} en los datos de la fila para formatear la plantilla.")
+            return f"ERROR_FORMATTING_MISSING_KEY_{e}"
+        except Exception as e:
+            print(f"Error inesperado al formatear: {e}")
+            return "ERROR_FORMATTING_UNEXPECTED"
